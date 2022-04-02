9c4b868dd823503a9de84702b08149879d3ce2d6
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-04-02 16:30:50 -0400

    Basic handlebars working

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes');
 // const dashboardRoutes = require('./dashboard-routes');
 
 const router = require('express').Router();
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 // router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use((req, res) => {
