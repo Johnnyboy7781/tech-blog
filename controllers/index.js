434a3a38c087cce69081e5b6a8e95df78126f13d
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-04-02 15:02:34 -0400

    Server finished

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes');
+// const dashboardRoutes = require('./dashboard-routes');
+
+const router = require('express').Router();
+
+// router.use('/', homeRoutes);
+// router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
